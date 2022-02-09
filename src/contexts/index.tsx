b65b32d9e5efe768610caab6d09e553f82c4605a
kimuichan <leerabi577@gmail.com>
2022-02-10 03:13:31 +0900

    ⚡️ :: (#6) context provider combine 함수 추가

---
+++ src/contexts/index.tsx
@@ -0,0 +1,29 @@
+import { Provider } from "react";
+
+const provider = <T extends {}>(
+  provider: Provider<T>,
+  props: T
+): ProvidersType<T> => ({
+  provider,
+  props,
+});
+
+const providers: ProvidersType<any>[] = [
+  /* 사용법 :
+        provider(loginProvider :(context provider), LoginDefaultValue :(context defualt value)),
+*/
+];
+
+type ProvidersType<T> = {
+  provider: Provider<T>;
+  props: T;
+};
+
+const RootContextProvider: React.FC = ({ children }) => {
+  providers.reduceRight((acc, Comp) => {
+    return <Comp.provider value={Comp.props}>{acc}</Comp.provider>;
+  }, children);
+  return <>{children}</>;
+};
+
+export default RootContextProvider;
