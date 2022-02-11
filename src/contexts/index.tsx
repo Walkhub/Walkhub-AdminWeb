6e0f439d70159afcdbb2741540c6a06ccf46b350
kimuichan <leerabi577@gmail.com>
2022-02-11 23:01:52 +0900

    ♻️ :: root provider 파일 위치 변환 및 context 로직 변경

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,16 +1,1 @@
-import React from "react";
-
-const RootContextProvider: React.FC<{ providers?: React.FC[] }> = ({
-  children,
-  providers = [],
-}) => {
-  return (
-    <>
-      {providers.reduceRight((acc, Comp) => {
-        return <Comp>{acc}</Comp>;
-      }, children)}
-    </>
-  );
-};
-
-export default RootContextProvider;
+export { default as LoginContextProvider } from "./LoginContext";
