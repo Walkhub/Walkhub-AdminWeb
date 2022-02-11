e4f6726b636eb0579c2ac4bc4652e7198f6f8c35
Yeong joon <bluehome8626@naver.com>
2022-02-12 01:30:53 +0900

    ⚡:: (#13) Notice Context 예시

--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -11,11 +11,40 @@ const NoticeWrite = () => {
   const NoticeInfoOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     if (name === "title" || name === "scope")
-      dispatch({ type: "NOTICE_INPUT", inputInfo: "title", value: value });
+      dispatch({ type: "NOTICE_INPUT", inputInfo: name, value: value });
   };
+
+  const NoticeContentOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value, name } = e.target;
+    if (name === "scope")
+      dispatch({ type: "NOTICE_INPUT", inputInfo: name, value: value });
+  };
+
+  return (
+    <div>
+      <div>title:{state.title}</div>
+      <div>content:{state.content}</div>
+      <div>scope:{state.scope}</div>
+      <input
+        value={state.title}
+        onChange={NoticeInfoOnChange}
+        name="id"
+        placeholder="id"
+      />
+      <textarea
+        value={state.content}
+        onChange={NoticeContentOnChange}
+        name="password"
+        placeholder="password"
+      />
+      <input
+        value={state.scope}
+        onChange={NoticeInfoOnChange}
+        name="scope"
+        placeholder="scope"
+      />
+    </div>
+  );
 };
 
-const NoticeContentInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  const { value, name } = e.target;
-  dispatch({ type: "NOTICE_INPUT", inputInfo: "content", value: value });
-};
+export default NoticeWrite;
