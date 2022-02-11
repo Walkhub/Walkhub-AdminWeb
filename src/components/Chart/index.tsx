d952f2716068879e5df4a8f986ca8974aeabfce6
oajh04 <cloudtubekr@naver.com>
2022-02-12 02:11:00 +0900

    ⚡️ :: (#18) chart 컴포넌트

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import useChartData from '../../hooks/useChartData';
+
+interface Props {
+    countList: number[]
+}
+
+const Chart: FC<Props> = ({countList}) => {
+    const chartData = useChartData(countList)
+
+    return (
+        <>
+        {
+            <LineChart width={460} height={230} data={chartData}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="1" vertical={false}/>
+                <XAxis dataKey="date" padding={{left: 20, right: 20}} />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="count" stroke="#4D99F0" />
+            </LineChart>
+        }
+        </>
+    );
+}
+
+export default Chart
