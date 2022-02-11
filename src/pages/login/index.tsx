aa1daffa3bedcd55268d19dea3aa4fe50dc61851
kimuichan <leerabi577@gmail.com>
2022-02-11 11:49:50 +0900

    ⚡️ :: (#11) login context 간단하게 예시

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import {
+  LoginDispatchContext,
+  LoginStateContext,
+} from "../../contexts/LoginContext";
+
+const Login = () => {
+  const state = useContext(LoginStateContext);
+  const dispatch = useContext(LoginDispatchContext);
+
+  const loginInfoOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    if (name === "id" || name === "password")
+      dispatch({ type: "Input", inputInfo: name, value: value });
+  };
+
+  return (
+    <div>
+      <div>id:{state.id}</div>
+      <div>password:{state.password}</div>
+      <input
+        value={state.id}
+        onChange={loginInfoOnChange}
+        name="id"
+        placeholder="id"
+      />
+      <input
+        value={state.password}
+        onChange={loginInfoOnChange}
+        name="password"
+        placeholder="password"
+      />
+    </div>
+  );
+};
+
+export default Login;
