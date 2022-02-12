4ba9cf6c14b83217b39042075436b7247fdac680
oajh04 <cloudtubekr@naver.com>
2022-02-12 19:01:51 +0900

    ⚡️ :: (#18) 날짜 수정

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -1,13 +1,15 @@
 const useChartData = (data: number[]) => {
     const chartData: { date: string; count: number }[] = []
-    const today = new Date()
-
-    data.map((i: number, idx) => chartData.push(
+    data.map((i: number, idx) => {
+        const now = new Date()
+        const chartDate = new Date(now.setDate(now.getDate() - (data.length-idx-1)));
+        console.log(chartDate.getMonth(), chartDate.getDate())
+        chartData.push(
         {
-            date: `${today.getMonth() + 1} / ${today.getDate() - (6-idx)}`,
+            date: `${chartDate.getMonth()+1}/${chartDate.getDate()}`,
             count: i
         })
-    )
+    })
     
     return chartData
 }
