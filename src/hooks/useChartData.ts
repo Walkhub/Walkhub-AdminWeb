0a8f41b862ec5a10b1e434974ac4fc0649afcbbc
oajh04 <cloudtubekr@naver.com>
2022-02-13 03:08:49 +0900

    ⚡️ :: (#31) 일주일 마다 인덱스 넣어주기

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -1,14 +1,16 @@
 const useChartData = (data: number[]) => {
-  const chartData: { date: string; count: number }[] = [];
+  const chartData: { date: string; count: number; color: number }[] = [];
+  let colorIndex = 0;
   data.map((i: number, idx) => {
     const now = new Date();
     const chartDate = new Date(
       now.setDate(now.getDate() - (data.length - idx - 1))
     );
-    console.log(chartDate.getMonth(), chartDate.getDate());
+    if (chartDate.getDay() === 1) colorIndex++;
     chartData.push({
       date: `${chartDate.getMonth() + 1}/${chartDate.getDate()}`,
       count: i,
+      color: colorIndex,
     });
   });
 
