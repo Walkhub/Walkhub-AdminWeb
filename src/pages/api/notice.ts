58d8a61a53c6cdfe55a449fbdd2503d5366f90d5
oajh04 <cloudtubekr@naver.com>
2022-02-14 00:03:07 +0900

    ⚡️ :: (#35) url 제거

--- src/pages/api/notice.ts
+++ src/pages/api/notice.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const query = req.query.scope;
   const { data } = await instance.get(
-    `http://211.38.86.92:8080/notices/list?scope=${query}`,
+    `/notices/list?scope=${query}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
