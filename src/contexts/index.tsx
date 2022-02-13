d6e44e3924ef6ee297450144bb70626ce2d46b82
oajh04 <cloudtubekr@naver.com>
2022-02-14 00:00:16 +0900

    ⚡️ :: (#35) contexts index에 추가

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,3 +1,4 @@
 export { default as LoginContextProvider } from "./LoginContext";
 export { default as NoticeContextProvider } from "./NoticeContext";
 export { default as ClassPostContextProvider } from "./ClassPostContext";
+export { default as NoticeListContextProvider } from "./NoticeListContext";
