1289237a850bdfdfe8e81baf1c6ece78b014e8f1
oajh04 <cloudtubekr@naver.com>
2022-02-14 22:07:20 +0900

    ⚡️ :: (#31) 함수로 이동 및 로직 변경

---
+++ src/utils/function/getChartData.ts
@@ -0,0 +1,25 @@
+const getChartData = (data: number[]) => {
+  const chartData: { date: string; count: number; color: number }[] = [];
+  for (let i in data) {
+    const now = new Date();
+    const chartDate = new Date(
+      now.setDate(now.getDate() - (data.length - i - 1))
+    );
+
+    chartData.push({
+      date: `${chartDate.getMonth() + 1}/${chartDate.getDate()}`,
+      count: data[i],
+      color: getWeek(chartDate),
+    });
+  }
+
+  return chartData;
+};
+
+function getWeek(paramDate: Date) {
+  const day = paramDate.getDay();
+  const diff = paramDate.getDate() - day + (day == 0 ? -6 : 1);
+  return Math.ceil(diff / 7);
+}
+
+export default getChartData;
