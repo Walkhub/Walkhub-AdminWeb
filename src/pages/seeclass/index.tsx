28ae610810e3fd092124e68e72888315af4b2766
Yeong joon <bluehome8626@naver.com>
2022-02-17 08:43:36 +0900

    ⚰️ :: (#39) 불필요한 component 삭제

--- src/pages/seeclass/index.tsx
+++ src/pages/seeclass/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ClassBanner from "@src/components/class/seeClass/ClassBanner";
-import StudentList from "@src/components/class/seeClass/StudentList";
 import { SWRConfig } from "swr";
 import fetcher from "@src/utils/function/fetcher";
 
@@ -20,7 +19,6 @@ const SeeClass = ({ fallback }: any) => {
   return (
     <SWRConfig value={{ fallback }}>
       <ClassBanner />
-      <StudentList />
     </SWRConfig>
   );
 };
