d78e9117723c474aff35cc173c3b3bda39822c5e
Yeong joon <bluehome8626@naver.com>
2022-02-16 10:23:52 +0900

    📑 :: (#39) SeeClass page 작성

---
+++ src/pages/seeclass/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassBanner from "@src/components/class/seeClass/ClassBanner";
+import StudentList from "@src/components/class/seeClass/StudentList";
+
+const SeeClass = () => {
+  return (
+    <>
+      <ClassBanner />
+      <StudentList />
+    </>
+  );
+};
+
+export default SeeClass;
