24c20a7f4b8eb9b1c6862073b03185dceb06368e
강석현 <tjrgus0713@gmail.com>
2022-02-16 17:35:17 +0900

    ⚡️::(#47) prettier로 스타일 정리

--- src/components/common/dropdown/index.tsx
+++ src/components/common/dropdown/index.tsx
@@ -3,51 +3,69 @@ import OutsideClickHandler from "react-outside-click-handler";
 import styled from "@emotion/styled";
 
 interface PropsType {
-  width : number;
-  heigth : number;
-  selectedValue : string;
-  setSelectedValue : (value : string) => void;
-  optionList : string[];
+  width: number;
+  heigth: number;
+  selectedValue: string;
+  setSelectedValue: (value: string) => void;
+  optionList: string[];
 }
-const Dropdown:React.FC<PropsType> = ({width,heigth,selectedValue,setSelectedValue,optionList}) => {
-  const [isFold,setIsFold] = useState(false);
+const Dropdown: React.FC<PropsType> = ({
+  width,
+  heigth,
+  selectedValue,
+  setSelectedValue,
+  optionList,
+}) => {
+  const [isFold, setIsFold] = useState(false);
   const reverseDropdownStatus = () => {
-    setIsFold((prev)=>!prev)
-  }
+    setIsFold(prev => !prev);
+  };
   const OptionList = useMemo(
     () =>
-      optionList.map((item)=>(
-        <li className="eachOption" onClick={() => {
-          setSelectedValue(item);
-          reverseDropdownStatus()
-        }}>{item}</li>
-      )),[optionList]
-  )
+      optionList.map(item => (
+        <li
+          className='eachOption'
+          onClick={() => {
+            setSelectedValue(item);
+            reverseDropdownStatus();
+          }}
+        >
+          {item}
+        </li>
+      )),
+    [optionList]
+  );
   return (
     <OutsideClickHandler onOutsideClick={() => setIsFold(false)}>
-      <Wrapper width={width} heigth={heigth} isFold={isFold} className="dropdown">
-        <Selected  isDefaultValue={optionList.includes(selectedValue)}>{selectedValue}</Selected>
-        <button className="arrowButton" onClick={reverseDropdownStatus}>y</button>
+      <Wrapper
+        width={width}
+        heigth={heigth}
+        isFold={isFold}
+        className='dropdown'
+      >
+        <Selected isDefaultValue={optionList.includes(selectedValue)}>
+          {selectedValue}
+        </Selected>
+        <button className='arrowButton' onClick={reverseDropdownStatus}>
+          y
+        </button>
       </Wrapper>
-      {
-        isFold &&
-        <Options width={width}>
-          {OptionList}
-        </Options>
-      }
+      {isFold && <Options width={width}>{OptionList}</Options>}
     </OutsideClickHandler>
-  )
-}
+  );
+};
 
 const Wrapper = styled.label<{
-  width : number;
-  heigth : number;
-  isFold : boolean;
+  width: number;
+  heigth: number;
+  isFold: boolean;
 }>`
-  width: ${(props)=>props.width}px;
-  height: ${(props)=>props.heigth}px;
+  width: ${props => props.width}px;
+  height: ${props => props.heigth}px;
   border: 1px solid ${({ theme }) => theme.color.normal_gray};
-  border: 1px solid ${(props)=>props.isFold ? props.theme.color.main : props.theme.color.normal_gray};
+  border: 1px solid
+    ${props =>
+      props.isFold ? props.theme.color.main : props.theme.color.normal_gray};
   border-radius: 12px;
   display: flex;
   padding: 12px 0 12px 16px;
@@ -57,7 +75,7 @@ const Wrapper = styled.label<{
   font-weight: normal;
   text-align: left;
   position: relative;
-  
+
   > .arrowButton {
     width: 16px;
     height: 16px;
@@ -67,23 +85,26 @@ const Wrapper = styled.label<{
     background-color: transparent;
     cursor: pointer;
   }
-`
+`;
 const Selected = styled.div<{
-  isDefaultValue : boolean;
+  isDefaultValue: boolean;
 }>`
   width: calc(100% - 28px);
   height: 100%;
-  color: ${(props)=>props.isDefaultValue ? props.theme.color.black : props.theme.color.normal_gray};
-`
+  color: ${props =>
+    props.isDefaultValue
+      ? props.theme.color.black
+      : props.theme.color.normal_gray};
+`;
 
 const Options = styled.ul<{
-  width : string
+  width: string;
 }>`
-  width: ${(props)=>props.width}px;
+  width: ${props => props.width}px;
   position: absolute;
   border-radius: 12px;
-  border: 1px solid ${({theme})=>theme.color.normal_gray};
-  background-color: ${({theme})=>theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.normal_gray};
+  background-color: ${({ theme }) => theme.color.white};
   margin-top: 12px;
   > .eachOption {
     width: 100%;
@@ -92,14 +113,14 @@ const Options = styled.ul<{
     display: flex;
     justify-content: center;
     align-items: center;
-    border-bottom: 1px solid ${({theme})=>theme.color.normal_gray};
+    border-bottom: 1px solid ${({ theme }) => theme.color.normal_gray};
     :hover {
-      color: ${({theme})=>theme.color.main};
+      color: ${({ theme }) => theme.color.main};
     }
     :last-child {
       border-bottom: none;
     }
   }
-`
+`;
 
 export default Dropdown;
