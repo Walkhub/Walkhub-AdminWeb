71f4edf74b8655d29ebdfdba20ae77870d730d73
Yeong joon <bluehome8626@naver.com>
2022-02-16 10:07:34 +0900

    ⚡:: (#39) Student Form 퍼블리싱

--- src/components/class/seeClass/StudentForm.tsx
+++ src/components/class/seeClass/StudentForm.tsx
@@ -16,10 +16,10 @@ const StudentForm = () => {
         </ProfileInfoDiv>
       </ProfileDiv>
       <ReportDiv>
-        <p>12000</p>
-        <p>2000000</p>
-        <p>0.1</p>
-        <p>0.12</p>
+        <p style={{ margin: "0 0 0 10px" }}>12000</p>
+        <p style={{ margin: "0 10px 0 0" }}>200000</p>
+        <p style={{ margin: "0 0 0 0" }}>11.1</p>
+        <p style={{ margin: "0 0 0 20px" }}>1000</p>
       </ReportDiv>
     </Wrapper>
   );
@@ -28,10 +28,11 @@ const StudentForm = () => {
 const Wrapper = styled.div`
   width: 100%;
   height: 100px;
-  padding: 20px 150px 20px 20px;
+  padding: 20px 160px 20px 20px;
   border: 1px solid ${({ theme }) => theme.color.normal_gray};
   border-radius: 12px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
 `;
 
@@ -70,11 +71,13 @@ const StudentInfo = styled.div`
 `;
 
 const ReportDiv = styled.div`
-  width: 596px;
+  width: 590px;
   display: flex;
   justify-content: space-between;
   > p {
     font-size: 20px;
+    font-style: normal;
+    font-weight: medium;
     color: ${({ theme }) => theme.color.black};
   }
 `;
