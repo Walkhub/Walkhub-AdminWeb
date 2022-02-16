c38b13ec92f26440decc7605057c288d3b5baef7
Yeong joon <bluehome8626@naver.com>
2022-02-16 15:53:00 +0900

    📑:: (#60) Notice 파일 생성

---
+++ src/components/notice/NoticeForm.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import styled from "@emotion/styled";
+
+const NoticeForm: FC = () => {
+  return <></>;
+};
+
+export default NoticeForm;
---
+++ src/components/notice/NoticeList.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import styled from "@emotion/styled";
+
+const NoticeList: FC = () => {
+  return <></>;
+};
+
+export default NoticeList;
