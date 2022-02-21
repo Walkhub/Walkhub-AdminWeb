b6d5924bcfe04ada2f301d2fd6fff08f8e0f52a9
oajh04 <cloudtubekr@naver.com>
2022-02-21 16:35:32 +0900

    ⚡️ :: findIndex를 find 로 수정

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -1,30 +1,26 @@
 // /* eslint-disable import/no-anonymous-default-export */
-import useAuthority from "@src/hooks/useAuthrity";
 import ToastError from "@src/utils/function/errorMessage";
 import { getToken } from "@src/utils/function/tokenManager";
-import { AuthrityType } from "@src/utils/interfaces/auth";
+import { AuthorityType } from "@src/utils/interfaces/auth";
 import { useRouter } from "next/dist/client/router";
 import { useLayoutEffect } from "react";
 
 function withAuth<T>(
   Component: React.ComponentType<T>,
-  option: AuthrityType[]
+  option: AuthorityType[]
 ) {
   const AuthenticateCheck = (props: T) => {
-    // const authState = useAuthority();
     const router = useRouter();
 
     useLayoutEffect(() => {
       if (!getToken().accessToken && !getToken().refreshToken) {
         router.push("/login");
         ToastError("로그인을 해주세요");
-      } else if (localStorage.getItem("authrity") === "USER") {
+      } else if (localStorage.getItem("authority") === "USER") {
         router.push("/login/certification");
         ToastError("접근 권한이 없습니다.");
       } else if (
-        option.findIndex(
-          (i: string) => i === localStorage.getItem("authrity")
-        ) === -1
+        !option.find((i: string) => i === localStorage.getItem("authority"))
       ) {
         router.back();
         ToastError("접근 권한이 없습니다.");
