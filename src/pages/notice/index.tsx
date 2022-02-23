eea1e327101146cb9faf87eb5b89cff003a9ed45
Yeong joon <bluehome8626@naver.com>
2022-02-23 22:19:22 +0900

    ⚡:: (#60) NoticePage 추가

---
+++ src/pages/notice/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MakeNotice from "@src/components/notice/MakeNotice";
+import NoticeForm from "@src/components/notice/NoticeForm";
+
+const Notice = () => {
+  return (
+    <>
+      <MakeNotice />
+    </>
+  );
+};
