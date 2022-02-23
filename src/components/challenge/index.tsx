7887ec96c1ba932cdc029ccd193137cb6daee469
강석현 <tjrgus0713@gmail.com>
2022-02-23 15:14:40 +0900

    ⚡️::(#66) 챌린지 생성 api 연동

--- src/components/challenge/index.tsx
+++ src/components/challenge/index.tsx
@@ -1,24 +1,28 @@
 import React, { ChangeEvent, useCallback, useState } from "react";
 import styled from "@emotion/styled";
 import TextField from "@src/components/challenge/textField";
-import {
-  ChallengeContentType,
-  userScopeType,
-} from "@src/utils/interfaces/challenge";
+import { ChallengeContentType } from "@src/utils/interfaces/challenge";
 import UserScope from "@src/components/challenge/userScope";
 import ImageUpload from "@src/components/challenge/imageUpload";
 import Goal from "@src/components/challenge/goal";
 import DefaultBtn from "@src/components/common/defaultBtn/DefaultBtn";
 import { createChallenge } from "@src/utils/apis/challenges";
 import { createImage } from "@src/utils/apis/default";
-import global from "@src/styles/global";
+import ToastError from "@src/utils/function/errorMessage";
+import axios from "axios";
+import { useRouter } from "next/dist/client/router";
+import { PageType } from "@src/pages/challenge";
 
-const Challenge: React.FC = () => {
+interface PropsType {
+  PageType : PageType;
+}
+
+const Challenge: React.FC<PropsType> = ({PageType}) => {
   const [challengeContent, setChallengeContent] =
     useState<ChallengeContentType>({
       name: "",
       content: "",
-      image_url: '',
+      image_url: "",
       start_at: "",
       end_at: "",
       award: "",
@@ -29,26 +33,20 @@ const Challenge: React.FC = () => {
       success_standard: null,
       grade: null,
     });
-  const [file,setFile] = useState<File | null>(null);
-  const {
-    name,
-    content,
-    image_url,
-    start_at,
-    end_at,
-    award,
-    user_scope,
-    goal,
-    goal_type,
-    goal_scope,
-    success_standard,
-    grade,
-  } = challengeContent;
-
+  const [file, setFile] = useState<File | string>("");
+  const { name, content, start_at, end_at, award } = challengeContent;
+  const router = useRouter();
   const onChangeInputValue = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       if (e.target.files) {
         setFile(e.target.files[0]);
+        return;
+      }
+      if (e.target.name == "goal" || e.target.name === "success_standard") {
+        setChallengeContent({
+          ...challengeContent,
+          [e.target.name]: Number(e.target.value),
+        });
         return;
       }
       setChallengeContent({
@@ -58,30 +56,61 @@ const Challenge: React.FC = () => {
     },
     [challengeContent]
   );
-  const onChangeDropdownValue = (value: string,name : string) => {
+  const onChangeDropdownValue = (value: string, name: string) => {
     setChallengeContent({
       ...challengeContent,
       [name]: value,
     });
   };
-  const onClickMakeChallenge = useCallback( async () => {
-    if(!file) return;
-    const formData = new FormData();
-    formData.append('images',file)
-    await createImage(formData).then(res => {
-      setChallengeContent({
+  const onClickMakeChallenge = useCallback(async () => {
+    try {
+      const formData = new FormData();
+      formData.append("images", file);
+      await createImage(formData).then(res => {
+        setChallengeContent({
+          ...challengeContent,
+          ["image_url"]: res.data.image_url[0],
+        });
+      });
+      createChallenge({
         ...challengeContent,
-        ['image_url'] : res.data.image_url[0]
-      })
-    })
-    createChallenge(challengeContent).then( res => {
-      console.log(res)
-    })
-  },[challengeContent,file])
+        ["start_at"]: `${start_at.substr(0, 4)}-${start_at.substr(
+          4,
+          2
+        )}-${start_at.substr(6, 2)}`,
+        ["end_at"]: `${end_at.substr(0, 4)}-${end_at.substr(
+          4,
+          2
+        )}-${end_at.substr(6, 2)}`,
+      });
+      success_handler();
+    } catch (err) {
+      errorHandler(err);
+    }
+  }, [challengeContent, file]);
+  const success_handler = () => {
+    router.push("/");
+  };
+  const errorHandler = (e: unknown) => {
+    if (axios.isAxiosError(e) && e.response) {
+      switch (e.response.status) {
+        case 400:
+          return ToastError("모든 빈칸을 채워주세요.");
+        case 401:
+          ToastError("로그인 상태를 다시 확인해 주세요.");
+        case 403:
+          ToastError("챌린지를 생성할 수 있는 권한이 없습니다.");
+        case 500:
+          return ToastError("관리자에게 문의해주세요");
+      }
+    } else {
+      ToastError("네트워크 연결을 확인해주세요.");
+    }
+  };
   return (
     <Wrapper>
       <ChallengeBox>
-        <header className='challengeHeader'>챌린지 생성</header>
+        <header className='challengeHeader'>챌린지 {PageType}</header>
         <InputsArea>
           <TextField
             width={392}
@@ -93,11 +122,11 @@ const Challenge: React.FC = () => {
             type='text'
             inputName='name'
           />
-          <UserScope changeChallengeContent={onChangeDropdownValue} challengeContent={challengeContent} />
-          <ImageUpload
-            onChangeInputValue={onChangeInputValue}
-            value={file}
+          <UserScope
+            changeUserScopeValue={onChangeDropdownValue}
+            challengeContent={challengeContent}
           />
+          <ImageUpload onChangeInputValue={onChangeInputValue} value={file} />
           <TextField
             width={392}
             disabled={false}
@@ -144,7 +173,9 @@ const Challenge: React.FC = () => {
             type='text'
           />
         </InputsArea>
-        <DefaultBtn onClick={onClickMakeChallenge} width={184}>생성하기</DefaultBtn>
+        <DefaultBtn onClick={onClickMakeChallenge} width={184}>
+          생성하기
+        </DefaultBtn>
       </ChallengeBox>
     </Wrapper>
   );
@@ -176,7 +207,7 @@ const ChallengeBox = styled.section`
     font-weight: bold;
     font-style: normal;
   }
-  > button{
+  > button {
     margin-top: 44px;
     display: flex;
     justify-content: center;
