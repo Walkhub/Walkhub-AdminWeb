bec422bd567467b23d684adbf782530b26eb246d
Yeong joon <bluehome8626@naver.com>
2022-02-23 22:18:21 +0900

    ⚡:: (#60) Default Btn 수정

--- src/components/common/defaultBtn/DefaultBtn.tsx
+++ src/components/common/defaultBtn/DefaultBtn.tsx
@@ -1,26 +1,36 @@
-import React, { FC, HTMLAttributes } from "react";
+import React, { FC, InputHTMLAttributes } from "react";
 import styled from "@emotion/styled";
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   width?: number;
+  defaultColor?: boolean;
 }
 
-const DefaultBtn: FC<Props> = props => {
-  return <DefaultBtnBox {...props}>{props.children}</DefaultBtnBox>;
+const DefaultBtn = ({
+  defaultColor = true,
+  type = "button",
+  ...props
+}: Props) => {
+  return <DefaultBtnBox type={type} defaultColor={defaultColor} {...props} />;
 };
 
-const DefaultBtnBox = styled.button<{ width?: number }>`
+const DefaultBtnBox = styled.input<{ width?: number; defaultColor: boolean }>`
   cursor: pointer;
   width: ${({ width }) => (width ? `${width}px` : `100%`)};
   height: 48px;
-  background: ${({ theme }) => theme.color.main};
+  background: ${({ defaultColor, theme }) =>
+    defaultColor ? theme.color.main : theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.main};
   border-radius: 8px;
-  color: ${({ theme }) => theme.color.white};
+  color: ${({ defaultColor, theme }) =>
+    defaultColor ? theme.color.white : theme.color.main};
   transition: all 0.3s;
   :hover {
-    background: ${({ theme }) => theme.color.white};
+    background: ${({ defaultColor, theme }) =>
+      defaultColor ? theme.color.white : theme.color.main};
     border: 1px solid ${({ theme }) => theme.color.main};
-    color: ${({ theme }) => theme.color.main};
+    color: ${({ defaultColor, theme }) =>
+      defaultColor ? theme.color.main : theme.color.white};
   }
 `;
 
