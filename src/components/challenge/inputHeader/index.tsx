7a313ee6084f991059b0673c73f2d5e307a55920
강석현 <tjrgus0713@gmail.com>
2022-02-23 16:00:16 +0900

    ⚡️::(#66) 인풋 설명나타내는 컴퍼넌트 추가

---
+++ src/components/challenge/inputHeader/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+interface PropsType {
+  disabled: boolean;
+}
+
+const InputHeader: React.FC<PropsType> = ({ children, disabled }) => {
+  return (
+    <Wrapper disalbed={disabled}>
+      <p className='text'>{children}</p>
+      <p className='required'>*</p>
+    </Wrapper>
+  );
+};
+export default InputHeader;
+
+const Wrapper = styled.section<{
+  disalbed: boolean;
+}>`
+  display: flex;
+  font-size: 16px;
+  line-height: 24px;
+  font-style: normal;
+  font-weight: normal;
+  margin-bottom: 12px;
+  > .text {
+    color: ${props =>
+      props.disalbed ? props.theme.color.normal_gray : props.theme.color.black};
+  }
+  > .required {
+    color: ${props =>
+      props.disalbed ? props.theme.color.normal_gray : "#4D99F0"};
+  }
+`;
