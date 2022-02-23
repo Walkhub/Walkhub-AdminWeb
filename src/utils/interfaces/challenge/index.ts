85178cf9bf76cb6641976f353dcbdd2575ac843e
강석현 <tjrgus0713@gmail.com>
2022-02-23 15:11:17 +0900

    ⚡️::(#66) interface 수정

--- src/utils/interfaces/challenge/index.ts
+++ src/utils/interfaces/challenge/index.ts
@@ -13,3 +13,23 @@ export interface ChallengeType {
     profile_image_url: string;
   };
 }
+export type userType = "TEACHER" | "ROOT" | "SU";
+export type userScopeType = "ALL" | "SCHOOL" | "GRADE" | "CLASS";
+export type gradeType = "1" | "2" | "3" | "4" | "5" | "6";
+export type goalType = "WALK" | "DISTANCE";
+export type goalScopeType = "ALL" | "DAY";
+
+export interface ChallengeContentType {
+  name: string;
+  content: string;
+  image_url: string;
+  start_at: string;
+  end_at: string;
+  award: string;
+  user_scope: userScopeType;
+  goal: number | null;
+  goal_type: goalType | null;
+  goal_scope: goalScopeType | null;
+  success_standard: number | null;
+  grade: number | null;
+}
