679657052771749f65b1192394a6fd75bf43f2c1
강석현 <tjrgus0713@gmail.com>
2022-02-23 15:12:46 +0900

    ⚡️::(#66) 입력창 설명 나타내는 컴퍼넌트 추가

--- src/components/challenge/inputHeader/index.tsx
+++ src/components/challenge/inputHeader/index.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 import styled from "@emotion/styled";
 
 interface PropsType {
-  disabled : boolean;
+  disabled: boolean;
 }
 
-const InputHeader:React.FC<PropsType> = ({children,disabled}) => {
+const InputHeader: React.FC<PropsType> = ({ children, disabled }) => {
   return (
     <Wrapper disalbed={disabled}>
-      <p className="text">{children}</p>
-      <p className="required">*</p>
+      <p className='text'>{children}</p>
+      <p className='required'>*</p>
     </Wrapper>
-  )
-}
-export default InputHeader
+  );
+};
+export default InputHeader;
 
 const Wrapper = styled.section<{
-  disalbed : boolean;
+  disalbed: boolean;
 }>`
   display: flex;
   font-size: 16px;
@@ -25,9 +25,11 @@ const Wrapper = styled.section<{
   font-weight: normal;
   margin-bottom: 12px;
   > .text {
-    color: ${props => props.disalbed ? props.theme.color.normal_gray : props.theme.color.black};
+    color: ${props =>
+      props.disalbed ? props.theme.color.normal_gray : props.theme.color.black};
   }
   > .required {
-    color: ${props => props.disalbed ? props.theme.color.normal_gray : "#4D99F0"};
+    color: ${props =>
+      props.disalbed ? props.theme.color.normal_gray : "#4D99F0"};
   }
-`
+`;
