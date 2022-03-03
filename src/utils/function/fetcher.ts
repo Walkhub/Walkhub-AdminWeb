0750f7f0a7060e67066c98dda511ef065950d615
Yeong joon <bluehome8626@naver.com>
2022-03-03 11:46:56 +0900

    ⚡:: (#60) headers 삭제

--- src/utils/function/fetcher.ts
+++ src/utils/function/fetcher.ts
@@ -2,12 +2,7 @@ import instance from "../axios";
 
 const fetcher = (url: string) => {
   return instance
-    .get(url, {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3anNkdWR3bnMiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjQ2Mjc0OTE1LCJleHAiOjE2NDYyODIxMTV9.bWVYcrOuxmJspGfprCgDbLpi9MJxhykXd0RqXTySSa4",
-      },
-    })
+    .get(url)
     .then(res => res.data)
     .catch(err => {
       return [];
