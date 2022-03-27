c4639212b221ec6c24ffbfd1694b0fe016ab6438
Yeong joon <bluehome8626@naver.com>
2022-03-27 23:30:53 +0900

    ⚡:: (#113) root 수정,결과 page 추가

---
+++ src/pages/su/modify/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ModifyRoot from "@src/components/su/ModifyRoot";
+
+const ModifyRootPage = () => {
+  return <ModifyRoot />;
+};
+
+export default ModifyRootPage;
---
+++ src/pages/su/result/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import RootResult from "@src/components/su/RootResult";
+
+const ResultPage = () => {
+  return <RootResult />;
+};
+
+export default ResultPage;
