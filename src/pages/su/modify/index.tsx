472521856667d4c06457cb5f9cfbcc5b842b4cba
Yeong joon <bluehome8626@naver.com>
2022-03-29 21:43:55 +0900

    ⚡:: (#113) 헤더 수정

--- src/pages/su/modify/index.tsx
+++ src/pages/su/modify/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ModifyRoot from "@src/components/su/ModifyRoot";
+import Header from "@src/components/common/header";
 
 const ModifyRootPage = () => {
-  return <ModifyRoot />;
+  return (
+    <>
+      <Header />
+      <ModifyRoot />
+    </>
+  );
 };
 
 export default ModifyRootPage;
