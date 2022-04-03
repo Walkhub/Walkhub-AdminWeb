8cbde70e1466e91fefc2949014d8996967dd95d5
Yeong joon <bluehome8626@naver.com>
2022-04-03 21:36:56 +0900

    ⚡:: (#113) 비밀번호 수정 api 추가

--- src/utils/apis/su/index.ts
+++ src/utils/apis/su/index.ts
@@ -7,3 +7,11 @@ export const createRoot = async (school_id: number) => {
     throw error;
   }
 };
+
+export const patchRootPassWord = async (school_id: number) => {
+  try {
+    await instance.patch(`/su/accounts/${school_id}`, { school_id });
+  } catch (error) {
+    throw error;
+  }
+};
