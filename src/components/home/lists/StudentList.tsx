b66f425565d0fd2add4695f8d0066537817eb54a
oajh04 <cloudtubekr@naver.com>
2022-05-07 19:12:46 +0900

    ⚡️ :: (#133) 학생 리스트 키값 수정

--- src/components/home/lists/StudentList.tsx
+++ src/components/home/lists/StudentList.tsx
@@ -1,20 +1,19 @@
 import fetcher from "@src/utils/function/fetcher";
 import { StudentType } from "@src/utils/interfaces/student";
-import React from "react";
+import React, { FC } from "react";
 import useSWR from "swr";
 import StudentCard from "../cards/StudentCard";
 
-const StudentList = () => {
-  const { data } = useSWR(
-    "/teachers/users?page=0&scope=ALL&sort=NAME&grade=&class=",
-    fetcher
-  );
+interface Props {
+  data: StudentType[];
+}
 
+const StudentList = ({ data }) => {
   return (
     <>
       {data.user_list?.map((i: StudentType) => {
         return (
-          <div style={{ marginBottom: "16px" }} key={i.user_id}>
+          <div style={{ marginBottom: "16px" }} key={`${i.name}-${i.user_id}`}>
             <StudentCard {...i} />
           </div>
         );
