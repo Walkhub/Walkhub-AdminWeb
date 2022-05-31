2d9957cec2bfcb1dea9ef71d2d357c95170af222
kimuichan <leerabi577@gmail.com>
2022-05-31 22:09:18 +0900

    ⚙️ :: (#118) image path 추가

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { esmExternals: false },
-}
+  images: {
+    domains: ["blog.kakaocdn.net", "walkhub.s3.ap-northeast-2.amazonaws.com"],
+  },
+};
